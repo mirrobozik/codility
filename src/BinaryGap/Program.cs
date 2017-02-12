7d952e4bd90ec4d5413556708eeb338ebb51e30d
Miroslav Božík <Miroslav Bok>
2017-02-12 16:44:31 +0100

    binary gap task

---
+++ src/BinaryGap/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MiroBozik.Codility.BinaryGap
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("missing input number");
+                Console.WriteLine("example: dotnet run 1041");
+                return;
+            }
+            int inputNumber = Convert.ToInt32(args[0]);
+
+            var solution = new Solution();
+
+            var result = solution.solution(inputNumber);
+
+            Console.WriteLine("RESULT: {0}", result);
+        }
+    }
+}
---
+++ src/BinaryGap/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace MiroBozik.Codility.BinaryGap
+{
+    public class Solution
+    {
+        public int solution(int input)
+        {
+            Console.WriteLine("input: {0}", input);
+            var binary = Convert.ToString(input, 2);
+            Console.WriteLine("input (binary): {0}", binary);
+            binary = binary.TrimEnd('0');
+            
+            var gabs = binary.Split(new[] {'1'}, StringSplitOptions.RemoveEmptyEntries);
+            if (gabs.Length == 0)
+            {
+                return 0;
+            }
+            var max = gabs.Select(g => g.Length).Max();
+            return max;
+        }
+    }
+}
